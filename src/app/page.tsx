d3427c648b4f411002528fe8dd190757b0ed4b2f
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-11-20 16:23:56 +0530

    Done with adding and deleting the todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
-
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+
+  const [todos, setTodos] = useState<string[]>([]);
+  const [todo, setTodo] = useState<string>("");
+
+  const add = () => {
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+
+  const deleteToDo = (index: number) => {
+    setTodos(todos.filter((_, i) => i !== index))
+    // "_" this is used to skip the first parameter and move to next, as in we don't care what's written in the todo but we want to check its index.
+  }
+
   return (
     <div>
-      <h1>To do app</h1>
+      <h1>To Do app</h1>
+      <input 
+        type='text'
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      
+      <button
+        onClick={() => add()}
+      >
+        Add
+      </button>
+
+      {todos.length > 0 && (
+        <ul>
+          {
+            todos.map((value, index) => (
+              <div key={index}>
+                  <li>{value}</li>
+                  <button onClick={() => deleteToDo(index)}>Delete</button> 
+              </div>
+            ))
+          }
+        </ul>
+      )}
     </div>
   );
 }
